d35dfa48c0131bf33a18ade55de06bdd13e369b6
lizhen <lizhen@zhumengyuan.com>
2021-11-04 14:27:34 +0800

    project init

---
+++ src/test/java/com/lz/springboot_demo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lz.springboot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
