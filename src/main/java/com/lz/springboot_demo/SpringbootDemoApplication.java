b7bbc9cfc7f88d1e2dbcbdb3aa123162da01477c
lizhen <lizhen@zhumengyuan.com>
2021-11-04 15:37:03 +0800

    https://spring.io/quickstart

--- src/main/java/com/lz/springboot_demo/SpringbootDemoApplication.java
+++ src/main/java/com/lz/springboot_demo/SpringbootDemoApplication.java
@@ -2,12 +2,25 @@ package com.lz.springboot_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * https://spring.io/quickstart
+ */
 @SpringBootApplication
+@RestController
 public class SpringbootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+    //访问地址：http://localhost:8080/hello?name=Amy
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 
 }
